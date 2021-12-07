dca99c8330d42e0469b5c1b8cb7942b9252b9380
BahadirAk <ak_bahad@hotmail.com>
2021-12-08 02:34:14 +0300

    PropertyType list and status list are added to update page.

--- EmlakOfisi/Pages/Agent/Update.cshtml.cs
+++ EmlakOfisi/Pages/Agent/Update.cshtml.cs
@@ -15,24 +15,28 @@ namespace EmlakOfisi.Pages.Agent
         private IAgentService _agentService;
 		public AdEntity Ad { get; set; }
 		public AgentEntity Agent { get; set; }
+		public string PropertyType { get; set; }
+		public string Status { get; set; }
 		public UpdateModel(IAdService adService, IAgentService agentService)
 		{
             _adService = adService;
             _agentService = agentService;
 		}
 		public void OnGet(int id)
-        {
+		{
 			Agent = _agentService.Find(x => x.Username == MyClaimTypes.CurrentUser.Username);
 			Ad = _adService.GetById(id);
+			PropertyType = Ad.PropertyType;
+			Status = Ad.Status;
 		}
-        public IActionResult OnPost(AdEntity ad)
+        public IActionResult OnPost(AdEntity ad, string propertyType, string status)
 		{
 			Agent = _agentService.Find(x => x.Username == MyClaimTypes.CurrentUser.Username);
 			var result = _adService.GetById(ad.Id);
 			result.ImagePath = ad.ImagePath;
 			result.Name = ad.Name;
-			result.PropertyType = ad.PropertyType;
-			result.Status = ad.Status;
+			result.PropertyType = propertyType;
+			result.Status = status;
 			result.M2 = ad.M2;
 			result.NumberOfRooms = ad.NumberOfRooms;
 			result.Price = ad.Price;
