b7902615bc8556d0f85d8d801bbbf442803f4c0e
BahadirAk <ak_bahad@hotmail.com>
2021-12-08 02:16:02 +0300

    PropertyType list and status list are added to add page.

--- EmlakOfisi/Pages/Agent/Add.cshtml.cs
+++ EmlakOfisi/Pages/Agent/Add.cshtml.cs
@@ -24,29 +24,27 @@ namespace EmlakOfisi.Pages.Agent
 		{
 			Agent = _agentService.Find(x => x.Username == MyClaimTypes.CurrentUser.Username);
 		}
-		public IActionResult OnPost(AdEntity ad)
+		public IActionResult OnPost(AdEntity ad, string propertyType, string status)
 		{
 			Agent = _agentService.Find(x => x.Username == MyClaimTypes.CurrentUser.Username);
-			if (ModelState.IsValid)
+			AdEntity entity = new AdEntity();
+			entity.ImagePath = ad.ImagePath == null ? "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg" : ad.ImagePath;
+			entity.No = Guid.NewGuid();
+			entity.Name = ad.Name;
+			entity.PropertyType = propertyType;
+			entity.Status = status;
+			entity.M2 = ad.M2;
+			entity.NumberOfRooms = ad.NumberOfRooms;
+			entity.Price = ad.Price;
+			entity.IsDeleted = false;
+			entity.AgentEntity = Agent;
+			if(ad.Name == null || propertyType == null || status == null)
 			{
-				AdEntity entity = new AdEntity();
-				entity.ImagePath = ad.ImagePath == null ? "https://www.kenyons.com/wp-content/uploads/2017/04/default-image-620x600.jpg" : ad.ImagePath;
-				entity.No = Guid.NewGuid();
-				entity.Name = ad.Name;
-				entity.PropertyType = ad.PropertyType;
-				entity.Status = ad.Status;
-				entity.M2 = ad.M2;
-				entity.NumberOfRooms = ad.NumberOfRooms;
-				entity.Price = ad.Price;
-				entity.IsDeleted = false;
-				entity.AgentEntity = Agent;
-				_adService.Add(entity);
-				return RedirectToPage("/Agent/MyAds", new { Id = Agent.Id });
-			}
-			else
-			{
+				ViewData["Message"] = "Ýlan Adý, Emlak Türü ve Ýlan Durumu girilmesi zorunlu alanlardýr!!!";
 				return Page();
 			}
+			_adService.Add(entity);
+			return RedirectToPage("/Agent/MyAds", new { Id = Agent.Id });
 		}
 	}
 }
